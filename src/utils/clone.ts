fad6bebd9d7dc016f5c8a153e1e8181f12704bfb
tada5hi <peter.placzek1996@gmail.com>
2023-05-28 19:14:13 +0200

    fix: better polyfill implementation for cloning arrays and objects

--- src/utils/clone.ts
+++ src/utils/clone.ts
@@ -30,6 +30,45 @@ const gT = (() => {
     throw new Error('unable to locate global object');
 })();
 
+export function polyfillClone<T>(input: T) {
+    const map = new WeakMap();
+
+    const fn = <A>(value: A) : A => {
+        if (Array.isArray(value)) {
+            if (map.has(value)) {
+                return map.get(value);
+            }
+
+            const cloned = [] as A;
+            map.set(value, cloned);
+
+            value.map((el) => (cloned as any[]).push(fn(el)));
+
+            return cloned;
+        }
+
+        if (isObject(value)) {
+            if (map.has(value)) {
+                return map.get(value);
+            }
+
+            const output = {} as A;
+            const keys = Object.keys(value);
+
+            map.set(value, output);
+            for (let i = 0; i < keys.length; i++) {
+                output[keys[i] as keyof A] = fn(value[keys[i]]);
+            }
+
+            return output;
+        }
+
+        return value;
+    };
+
+    return fn(input);
+}
+
 /* istanbul ignore next */
 export function clone<T>(value: T) : T {
     if (gT.structuredClone) {
@@ -37,15 +76,5 @@ export function clone<T>(value: T) : T {
     }
 
     /* istanbul ignore next */
-    if (isObject(value)) {
-        return { ...value };
-    }
-
-    /* istanbul ignore next */
-    if (Array.isArray(value)) {
-        return [...value] as T;
-    }
-
-    /* istanbul ignore next */
-    return value;
+    return polyfillClone(value);
 }
---
+++ test/unit/utils/clone.spec.ts
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2023.
+ * Author Peter Placzek (tada5hi)
+ * For the full copyright and license information,
+ * view the LICENSE file that was distributed with this source code.
+ */
+
+import {polyfillClone} from "../../../src";
+
+describe('src/utils/clone', function () {
+    it('should polyfill clone objects with circular reference', () => {
+        const foo : Record<string, any> = {bar: 'baz'};
+        foo.boz = foo;
+
+        const copy = polyfillClone(foo);
+        expect(copy).toEqual(foo);
+    });
+
+    it('should polyfill clone arrays with circular reference', () => {
+        const foo : any = ['bar'];
+        foo.push(foo);
+
+        const copy = polyfillClone(foo);
+        expect(copy).toEqual(foo);
+    })
+});
